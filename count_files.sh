12bd2efcc84864e292d6154dcdd20d7c9ebba981
Roman Degtyarev <guromka@gmail.com>
2023-01-08 19:18:54 +0100

    Added recursive file counting.

---
+++ count_files.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+INPUTDIR="${1}"
+echo Input: $INPUTDIR
+cd "${INPUTDIR}"
+echo Cur dir: $PWD
+
+find . -maxdepth 10 -type d -print0 | while read -d '' -r dir; do
+	num=$(find $dir -ls | wc -l);
+	printf "%5d files in directory %s\n" "$num" "$dir";
+done
+
